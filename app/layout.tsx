2fb0014c9e08df6d21906f41e95e84a680efe580
Bailey Say <baileyasay@gmail.com>
2026-08-22 13:35:57 -0700

    rain - animation, center text; todo: moving border, dynamic rain drops

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Anta } from "next/font/google";
 import "./globals.css";
+
+const anta = Anta({
+  variable: "--font-anta",
+  subsets: ["latin"],
+  weight: "400",
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +27,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${anta.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
---
+++ components/Rain.tsx
@@ -0,0 +1,15 @@
+export default function Rain() {
+	return (
+		<div className="relative w-screen h-dvh border-1">
+			<div className="rain-cover">
+				<div className="rain-cover-left" />
+				<div className="rain-cover-right" />
+			</div>
+			<div className="rain-drops">Rain Drops</div>
+			<div className="rain-text">
+				<div className="rain-text-catch">Never Compromise On Yourself</div>
+				<div className="rain-text-site">baysalt.dev</div>
+			</div>
+		</div>
+	);
+}
